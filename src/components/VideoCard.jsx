71b0b0057e605b1a39597f4b7de1421aeef7d6cd
paran22 <knospe1@naver.com>
2023-08-22 15:38:01 +0900

    Fix: fix data format using timeago

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import { format } from "timeago.js";
+import { formatKo } from "../utils/timeago";
 
 export default function VideoCard({ video }) {
+  const { thumbnails, channelTitle, title, publishedAt } = video.snippet;
   return (
-    <div className="flex flex-col aspect-[5/4] mx-4 my-3 max-w-md max-h-96">
+    <li className="flex flex-col aspect-[5/4] mx-4 my-3 max-w-md max-h-96">
       <img
         className="rounded-lg object-cover w-full h-4/6"
-        src={video.snippet.thumbnails.medium.url}
+        src={thumbnails.medium.url}
         alt={video.description}
       />
       <div className="flex grow- px-2 py-2 gap-2 items-start">
         <img
           className="w-9 h-9 rounded-full"
-          src={video.snippet.thumbnails.medium.url}
-          alt={video.snippet.channelTitle}
+          src={thumbnails.medium.url}
+          alt={channelTitle}
         />
         <div className="flex-col">
-          <p className="text-base md:text-lg">{video.snippet.title}</p>
-          <p className="text-xs md:text-base">{video.snippet.channelTitle}</p>
-          <p className="text-xs md:text-base">{video.snippet.publishTime}</p>
+          <p className="text-base md:text-lg">{title}</p>
+          <p className="text-xs md:text-base">{channelTitle}</p>
+          <p className="text-xs md:text-base">{formatKo(publishedAt)}</p>
         </div>
       </div>
-    </div>
+    </li>
   );
 }
