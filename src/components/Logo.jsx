27dc64345875808b0f6620e1defd6efcbc72ace5
paran22 <knospe1@naver.com>
2023-08-22 11:25:02 +0900

    Feat: link Logo to index page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { GrPersonalComputer } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <div className="flex gap-2 items-center text-light">
-      <GrPersonalComputer className="text-5xl bg-light px-1" />
-      <span className="text-xl">Developer Youtube</span>
-    </div>
+    <Link to='/'>
+      <div className="flex gap-2 items-center text-light">
+        <GrPersonalComputer className="text-5xl bg-light px-1" />
+        <span className="text-xl">Developer Youtube</span>
+      </div>
+    </Link>
   );
 }
