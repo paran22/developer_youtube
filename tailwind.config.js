a660a2ce752e64340e56fbf61db59476a17f9b47
paran22 <knospe1@naver.com>
2023-08-22 11:54:47 +0900

    Fix: fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
       background: '#000000',
