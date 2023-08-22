c49aaf1b42b04b1357a64977b9a8d2a5229c0b0e
paran22 <knospe1@naver.com>
2023-08-22 16:25:01 +0900

    Fix: fix timago locale setting

--- src/utils/timeago.js
+++ src/utils/timeago.js
@@ -1,25 +1,8 @@
 import { register, format } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko';
 
-const localeFunc = (number, index, totalSec) => {
-  return [
-    ['방금 전', 'right now'],
-    ['%s초 전', 'in %s seconds'],
-    ['1분 전', 'in 1 minute'],
-    ['%s분 전', 'in %s minutes'],
-    ['1시간 전', 'in 1 hour'],
-    ['%s 시간 전', 'in %s hours'],
-    ['1일 전', 'in 1 day'],
-    ['%s일 전', 'in %s days'],
-    ['1주 전', 'in 1 week'],
-    ['%s주 전', 'in %s weeks'],
-    ['1개월 전', 'in 1 month'],
-    ['%s개월 전', 'in %s months'],
-    ['1년 전', 'in 1 year'],
-    ['%s년 전', 'in %s years']
-  ][index];
-};
-register('my-locale', localeFunc);
+register('ko', koLocale);
 
 export function formatKo(date) {
-  return format('2016-06-12', 'my-locale');
+  return format(date, 'ko');
 }
