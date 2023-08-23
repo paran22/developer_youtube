f29387f60c28347fd790ab48114cb5327b50ff95
paran22 <knospe1@naver.com>
2023-08-23 09:58:35 +0900

    Refactor: refactor VideoDetailPage

--- src/pages/VideoDetailPage.jsx
+++ src/pages/VideoDetailPage.jsx
@@ -4,7 +4,7 @@ import VideoDetailCard from "../components/VideoDetailCard";
 
 export default function VideoDetailPage() {
   const location = useLocation();
-  const channelId = { ...location.state.channelId };
+  const channelId = location.state.channelId;
   const videoId = location.state.vedioId;
   return (
     <section className="w-5/6 mx-auto py-4 flex">
