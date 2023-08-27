d9710505777606ddc238e6886fd9a68b78b5ae56
paran22 <knospe1@naver.com>
2023-08-28 08:39:28 +0900

    Config: add React query devtools

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from './components/Header';
 import { ApiContextProvider } from './context/ApiContext';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 const queryClient = new QueryClient();
 
@@ -13,6 +15,7 @@ function App() {
       <ApiContextProvider>
         <QueryClientProvider client={queryClient}>
           <Outlet />
+          <ReactQueryDevtools initialIsOpen={true} />
         </QueryClientProvider>
       </ApiContextProvider>
     </>
